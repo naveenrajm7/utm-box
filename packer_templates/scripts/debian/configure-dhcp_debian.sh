db1bcbc19cd1d7bb48aa8503f6454b795c26c474
Naveenraj M <22456988+naveenrajm7@users.noreply.github.com>
2025-01-31 16:24:42 -0700

    debian: fix interface names

--- packer_templates/scripts/debian/configure-dhcp_debian.sh
+++ packer_templates/scripts/debian/configure-dhcp_debian.sh
@@ -3,19 +3,19 @@
 # Backup existing network configuration
 cp /etc/network/interfaces /etc/network/interfaces.bak
 
-# Append new DHCP configuration for ens0s1 and ens0s2
+# Append new DHCP configuration for enp0s1 and enp0s2 
 cat <<EOF >> /etc/network/interfaces
 
-# Enable DHCP for ens0s1
-allow-hotplug ens0s1
-iface ens0s1 inet dhcp
+# Enable DHCP for enp0s1
+allow-hotplug enp0s1
+iface enp0s1 inet dhcp
 
-# Enable DHCP for ens0s2
-allow-hotplug ens0s2
-iface ens0s2 inet dhcp
+# Enable DHCP for enp0s2
+allow-hotplug enp0s2
+iface enp0s2 inet dhcp
 EOF
 
 # Restart networking service
 systemctl restart networking
 
-echo "DHCP has been enabled for ens0s1 and ens0s2. Please reboot if needed."
+echo "DHCP has been enabled for enp0s1 and enp0s2. Please reboot if needed."
